fd669fbe61f78920e48ef2654ea6cb6961639afc
Devansh Sharma <sharmadevansh2032@gmail.com>
2025-08-24 20:40:17 +0530

    learning input output

---
+++ input-output.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n; 
+  cout<<"Taking n as input: ";
+  cin>> n;
+  int m ;
+  cout<<"taking m as and input: "; cin>>m;
+  cout<<"value of n: "<<n + m;
+  return 0;
+};
