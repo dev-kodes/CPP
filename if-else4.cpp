cfcc84a2e0e23dd906be174f732fff6ef2a35ace
Devansh Sharma <sharmadevansh2032@gmail.com>
2025-08-25 13:24:23 +0530

    learning if else

---
+++ if-else4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int number;
+    cout<<"Enter your first number: ";
+    cin>>number;
+    
+    if (number%2 == 0)
+    {
+        cout<<"even";
+    }
+    else
+    {
+        cout<<"odd";
+    }
+}
