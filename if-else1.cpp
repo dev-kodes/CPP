252639ae461abc22c0c91ef7e691aba1b837c59d
Devansh Sharma <sharmadevansh2032@gmail.com>
2025-08-25 11:42:37 +0530

    learning if and else

---
+++ if-else1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int package;
+    // cout<<"Take package: ";
+    cin>>package;
+    
+    if (package>10) {
+    cout<<"Accepted";
+    }
+    else {
+        cout<<"Rejected";
+    }
+    return 0;
+};
