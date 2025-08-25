30cbeb929c99dddaf45afc535b89c747bc3b8211
Devansh Sharma <sharmadevansh2032@gmail.com>
2025-08-25 12:01:02 +0530

    learning if else

---
+++ if-else2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int Marks;
+    cin>> Marks;
+    if (Marks>33)
+    {
+        cout<<"Pass";
+    }
+    else {
+        cout<<"Fail";
+    }
+    return 0;
+};
