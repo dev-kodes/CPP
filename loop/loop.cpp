28509a4ffd83a7043d8b0ed3866c1aa0a06aa2fc
Devansh Sharma <sharmadevansh2032@gmail.com>
2025-08-26 11:53:19 +0530

    learning loop

--- loop/loop.cpp
+++ loop/loop.cpp
@@ -1,10 +1,10 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
-  int n;
-  cin>>n;
-  for(int i = 0;i <n ; i++){
-       cout<<i<<endl;
-  }
+    for (int i = 2; i<= 10; i = i+1)
+    {
+        cout<<"Hello\n";
+    }
+    
 }
