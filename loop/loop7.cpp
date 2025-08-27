5b7f677f8d7c42e7a6ec74572ccebf784a6ee090
Devansh Sharma <sharmadevansh2032@gmail.com>
2025-08-27 13:04:34 +0530

    learning loop

---
+++ loop/loop7.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n,i;
+    cout<<"Enter the number: ";
+    cin>>n;
+    for(i=n;i>=1;i=i-1) {
+        cout<<i<<" ";
+    }
+    return 0;
+};
