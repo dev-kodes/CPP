f805332c5440919c606dbcf1f33d079bd3d65957
Devansh Sharma <sharmadevansh2032@gmail.com>
2025-08-29 12:01:44 +0530

    learning pattern

---
+++ loop/pattern print/pattern8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int row;
+    char col;  for(row=1;row<=5;row=row+1) {
+        for(col='a';col<='e';col=col+1) 
+        {
+            cout<<col<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+};
