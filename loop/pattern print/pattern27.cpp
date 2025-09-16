772dc312c830d82b69e02676a11e59f45d4e4009
Devansh Sharma <sharmadevansh2032@gmail.com>
2025-09-16 09:23:17 +0530

    learning pattern

---
+++ loop/pattern print/pattern27.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()  {
+    int row,col,n;
+    cout<<"enter the number ";
+    cin>>n;
+    for(row=n;row>=1;row=row-1)
+    {
+        // space print
+        for(col=1;col<=n-row;col=col+1)  {
+            cout<<" "; }
+        // star print
+        for(col=1;col<=2*row-1;col=col+1) 
+        {
+            cout<<"*";
+        } 
+            cout<<endl;
+    }
+    return 0;
+};
