63fad08522e0c485f7d0e30294024b56f4191fd7
Devansh Sharma <sharmadevansh2032@gmail.com>
2025-09-17 12:19:49 +0530

    learning bodmas

---
+++ loop/pattern print/bodmas print/bodmas7.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()  {
+    // logical operator
+    
+    // int a,b,c;
+    // cin>>a>>b>>c;
+    
+    // if(a>b&&a>c)
+    // cout<<"yes";
+    // else
+    // cout<<"no";
+    
+    // or
+    
+    // char name = 'b';
+    // if(name=='a'||name=='e'|| name=='i' ||name=='o'||name=='u')
+    // cout<<"vowel";
+    // else
+    // cout<<"consonent";
+    
+    
+    cout<<!0<<endl;
+    
+};
