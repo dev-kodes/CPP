61906f0f332f4716a6d22557f464904e3400a87c
Devansh Sharma <sharmadevansh2032@gmail.com>
2025-09-17 11:19:23 +0530

    learning bodmas

---
+++ loop/pattern print/bodmas print/bodmas6.cpp
@@ -0,0 +1,27 @@
+using namespace std;
+int main()  {
+    // comparison operator
+    // 1 or 0
+    
+    int a,b;
+    cout<<"Enter the value of a: ";
+    cin>>a;
+    
+    cout<<"Enter the value of b: ";
+    cin>>b;
+    
+    // ==
+    
+    // if(a==b)
+    // cout<<"yes";
+    // else
+    // cout<<"no";
+    
+    // > , < , >= , <= , !=
+    
+    if(a!=b)
+    // why (4!=4 - becuase the statement is wroung)
+    cout<<"yes";
+    else
+    cout<<"no";
+};
