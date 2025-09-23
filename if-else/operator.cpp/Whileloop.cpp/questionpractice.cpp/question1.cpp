906055a5ffc0eda6cc556c225097c474695c96f2
Devansh Sharma <sharmadevansh2032@gmail.com>
2025-09-23 13:15:03 +0530

    learning if-else

---
+++ if-else/operator.cpp/Whileloop.cpp/questionpractice.cpp/question1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cout<<"enter two number: ";
+    cin>>a>>b;
+    if(a>b) {
+        cout<<"bigger number is: "<<a<<endl;
+    } else{
+        cout<<"bigger number is: "<<b<<endl;
+    }
+    return 0;
+};
