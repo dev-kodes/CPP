c48b8252cdc76aed2791fe44eb3767131887ca68
Devansh Sharma <sharmadevansh2032@gmail.com>
2025-09-23 11:20:38 +0530

    learning whileloop swtich

---
+++ if-else/operator.cpp/Whileloop.cpp/whileloopswitch8.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()  {
+int i;
+cout<<"Enter the number: ";
+cin>>i;
+switch(i)
+{
+    case 1:
+    cout<<"Rohit ";
+    break;
+    case 2:
+    cout<<"Mohit ";
+    break;
+    
+    default:
+    cout<<"Sohit ";
+    break;
+  }
+  return 0;
+};
