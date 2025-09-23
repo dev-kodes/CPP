dff54288bf2987b17f6fd033d8aa2c732d9a34f5
Devansh Sharma <sharmadevansh2032@gmail.com>
2025-09-23 11:35:13 +0530

    learning whileloop switch

---
+++ if-else/operator.cpp/Whileloop.cpp/whileloopswitch9.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()  {
+    int i;
+    cout<<"Enter the number: ";
+    cin>>i;
+    switch(i) {
+    case 1:
+    cout<<"Mon "<<endl;
+    
+    case 2:
+    cout<<"Tue "<<endl;
+    
+    case 3:
+    cout<<"Wed "<<endl;
+    
+    case 4:
+    cout<<"Thur "<<endl;
+    
+    case 5:
+    cout<<"Fri "<<endl;
+    
+    case 6:
+    cout<<"Sat "<<endl;
+        
+    case 7:
+    cout<<"Sun "<<endl;
+    }
+    return 0;
+};
